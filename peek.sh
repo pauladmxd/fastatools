b924f355365e18bed8790850921679258129d767
paula <pauladelgadomanzano@MBP-de-Paula.home>
2023-10-26 14:15:40 +0200

    Update peek.sh to accept two arguments

--- peek.sh
+++ peek.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Check if an input file is provided as a command-line argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <input_file>"
+# Check if two command-line arguments are provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <input_file> <number_of_lines>"
   exit 1
 fi
 
 input_file="$1"
+lines_to_display="$2"
 
 # Check if the input file exists
 if [ ! -f "$input_file" ]; then
@@ -14,12 +15,15 @@ if [ ! -f "$input_file" ]; then
   exit 1
 fi
 
-# Read the first three lines
-head -n 3 "$input_file"
+# Calculate the number of lines at the beginning and end
+lines_to_display=$((lines_to_display > 0 ? lines_to_display : 0))
+
+# Read the specified number of lines from the beginning
+head -n "$lines_to_display" "$input_file"
 
 # Print the "..." line
 echo "..."
 
-# Read the last three lines
-tail -n 3 "$input_file"
+# Read the specified number of lines from the end
+tail -n "$lines_to_display" "$input_file"
 
